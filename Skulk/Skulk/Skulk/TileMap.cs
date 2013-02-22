986fc96d5d2c715ae893b4ae544e2482f66ca284
Dylan Bray <d.bray@usask.ca>
2013-02-21 23:12:45 -0600

    more comments

--- Skulk/Skulk/Skulk/TileMap.cs
+++ Skulk/Skulk/Skulk/TileMap.cs
@@ -11,26 +11,21 @@ namespace Skulk
 
 		public TileMap ()
 		{
+            //parse spreadsheet for tiles
 			TextReader tr = new StreamReader("map.csv");
 			TextReader hr = new StreamReader("map.csv");
 			String mapLine = tr.ReadLine();
 			MapHeight = hr.ReadToEnd().Split('\n').Length - 1;
 			MapWidth = mapLine.Split(';').Length;
-			Console.WriteLine(MapHeight);
-			Console.WriteLine(MapWidth); 
 
 			mapCell = new MapCell[MapWidth,MapHeight];
 
 
 			for (int y = 0; y < MapHeight; y++)
             {
-
-				//Console.WriteLine(mapLine);
                 for (int x = 0; x < MapWidth; x++)
                 {
-					//Console.WriteLine(mapLine.Split(';')[x] + "x");
 					this.mapCell[x,y] = new MapCell(Convert.ToInt32(mapLine.Split(';')[x]));
-					//Console.WriteLine(mapCell[x,y].TileID);
                 }
 				mapLine = tr.ReadLine();
             }
